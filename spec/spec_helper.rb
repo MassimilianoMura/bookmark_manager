e8c68c9c735edc920617fe05e23985e0e7eb139d
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-29 17:36:26 +0100

    creates a 'new' page to add new links to the links' list page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require File.join(File.dirname(__FILE__), '..', 'lib/bookmark.rb')
 
 require './app/data_mapper_setup'
 
+require 'database_cleaner'
+
 
 require 'capybara'
 require 'capybara/rspec'
@@ -55,6 +57,22 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+
+    config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+
+
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
