cf758ad5360861e07237ed41ae96739649ca8867
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-29 18:54:03 +0100

    creates a tag class file

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,9 @@ require 'data_mapper'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager")
 
 require './lib/models/link'
+require './lib/models/tag'
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
